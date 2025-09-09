7a530a356d5251a92dfc7773cdc54527b14f4f3d
Junghyun Na <junghyun.na321@gmail.com>
2025-09-09 09:15:32 +0900

    [D3] Title: 증가하는 사탕 수열, Time: 59 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D3/20551. 증가하는 사탕 수열/증가하는 사탕 수열.py
+++ SWEA/D3/20551. 증가하는 사탕 수열/증가하는 사탕 수열.py
@@ -1,17 +1,26 @@
+#import sys
+#sys.stdin = open('input_20551.txt', 'r')
+
+
 def candy(A, B, C):
     # 절대적으로 불가능 -> -1 리턴
     if C < 3 or B < 2:
         return -1
 
     # 가능은 하다면, cnt 해보자.
+    # cnt 1 은 B를 먹는 개수
+    # cnt 2 는 A 를 먹는 개수
     cnt = 0
-    while B >= C:
-        cnt += 1
-        B -= 1
 
-    while A >= B:
-        cnt += 1
-        A -= 1
+    if B >= C:
+        cnt1 = B - C +1
+        cnt += cnt1
+        B -= cnt1
+
+    if A >= B:
+        cnt2 = A - B +1
+        cnt += cnt2
+        A -= cnt2
 
     return cnt
 
